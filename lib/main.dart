84f9527bf47bf976f844423d3362ccf9cb63837b
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-05-27 16:24:44 +0700

    authentication fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-
-import '../pages/favorite.dart';
-import '../pages/library.dart';
 import '../pages/signIn.dart';
 import '../pages/signUp.dart';
 import 'providers/storyAdd.dart';
 import '../providers/authentication.dart';
 import '../providers/favorites.dart';
 import '../providers/stories.dart';
-import '../navbar.dart';
 import '../wrapper.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //       debugShowCheckedModeBanner: false,
-  //       title: 'Flutter Demo',
-  //       theme: ThemeData(
-  //         primarySwatch: Colors.purple,
-  //       ),
-  //       home: signIn());
-  // }
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        Provider<Authentication>(
-          create: (_) => Authentication(),
-        ),
+        ChangeNotifierProvider(create: (context) => Authentication()),
         ChangeNotifierProvider(create: (context) => Stories()),
         ChangeNotifierProvider(create: (context) => StoryLists()),
         ChangeNotifierProvider(create: (context) => Favorites())

45135cc562e4fff665c34a89f54ea1b473dfb866
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-06-12 13:12:43 +0700

    uid on model added

--- lib/models/story_model.dart
+++ lib/models/story_model.dart
@@ -3,11 +3,20 @@ import 'package:flutter/material.dart';
 // model cerita asli
 
 class StoryList extends ChangeNotifier {
-  String id, title, description, writer, categories, imageUrl, part, content;
+  String id,
+      title,
+      description,
+      writer,
+      categories,
+      imageUrl,
+      part,
+      content,
+      uid;
   DateTime createdAt;
 
   StoryList(
       {required this.id,
+      required this.uid,
       required this.title,
       required this.description,
       required this.writer,
